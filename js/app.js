b011944447e123e0f3fdf0063bee81cc82c195ed
abssujan <abdussabursujan@gmail.com>
2024-01-08 20:21:42 +0600

    fetch phone data

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// fetch data
+const loadPhoneData = async() => {
+    const url =`https://openapi.programming-hero.com/api/phones?search=iphone`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneData(data.data)
+}
+const displayPhoneData = phones => {
+    console.log(phones)
+    const phoneContainer = document.getElementById('phone-container');
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML =`
+        <div class="card p-4">
+             <img src=" ${phone.image} " class="card-img-top w-75" alt="...">
+            <div class="card-body">
+                <h5 class="card-title"> ${phone.phone_name} </h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+             </div>
+        </div>
+        `;
+        phoneContainer.appendChild(phoneDiv)
+    })
+}
+loadPhoneData()
